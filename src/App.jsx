333b6b6f1d3af0e5ded868db41afdf614c63223c
sayedalali1 <sayedalali1@gmail.com>
2024-08-08 10:24:04 +0300

    buttons 3 & 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import './App.css';
 import Button1 from './components/example-1-normal-css/Button';
 import Button2 from './components/example-2-inline-css/Button';
+import Button3 from './components/example-3-css-in-js/Button';
+import Button4 from './components/example-4-styled-components/Button';
 
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
     <div className="App">
       <Button1 buttonText="Example 1" />
       <Button2 buttonText="Example 2" />
+      <Button3 buttonText="Example 3" />
+      <Button4 buttonText="Example 4" />
 
     </div>
   );
--- src/components/example-3-css-in-js/Button.jsx
+++ src/components/example-3-css-in-js/Button.jsx
@@ -1,8 +1,25 @@
 import { createUseStyles } from 'react-jss';
 
 const Button = ({ buttonText }) => {
+
+  const styles = createUseStyles({
+    btn: {
+      backgroundColor: '#24292e', // adding the styles in the object that we'll apply to our button
+      borderRadius: '8px',
+      color: '#f7df1c',
+      fontSize: '24pt',
+      padding: '15px 50px',
+      width: '300px',
+    },
+  });
+
+  const classes = styles();
   
-    return <button>{buttonText}</button>;
-  };
-  
-  export default Button;
+  return (
+    <button className={classes.btn}>
+      {buttonText}
+    </button>
+  );
+};
+
+export default Button;
